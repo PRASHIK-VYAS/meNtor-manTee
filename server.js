3fa2c31f0348fa94fedf251e38ca739b1d5af206
Prashik Nilesh Vyas <prashikvyas@gmail.com>
2026-04-05 11:53:04 +0530

    Make repo root boot the backend service

--- server.js
+++ server.js
@@ -1,18 +1,1 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Host the build output directory
-app.use(express.static(path.join(__dirname, 'build', 'web')));
-
-// Any path should fallback to the index.html
-app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'web', 'index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+require('./backend/server');
